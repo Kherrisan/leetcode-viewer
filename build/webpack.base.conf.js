f909f91e1d5c9c6e8a532fd5df346e4997bfef40
zoudikai <zdkscope@qq.com>
2019-02-12 16:43:01 +0800

    Adjust some deps version

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -65,7 +65,7 @@ module.exports = {
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-        loader: 'url',
+        loader: 'url?emitFile=false',
         query: {
           limit: 10000,
           name: utils.assetsPath('img/[name].[hash:7].[ext]')
@@ -73,7 +73,7 @@ module.exports = {
       },
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        loader: 'url',
+        loader: 'url?emitFile=false',
         query: {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
